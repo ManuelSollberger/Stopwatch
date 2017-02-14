bcd82de451da65bcfe7deffeb5a7a3f85401035b
Manuel <manuel@manuels-mac.local>
2017-02-14 13:12:13 +0100

    Started drawing the analog watch

--- Stopwatch/AnalogStopwatchView.m
+++ Stopwatch/AnalogStopwatchView.m
@@ -10,15 +10,18 @@
 
 @implementation AnalogStopwatchView
 
+float CLOCK_BORDER_DISTANCE = 16;
+float CLOCK_BORDER_SIZE = 1;
 
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
     CGContextRef g = UIGraphicsGetCurrentContext();
-    CGContextSetFillColor(g, CGColorGetComponents([[UIColor blackColor] CGColor]));
-    CGContextAddEllipseInRect(g, rect);
-    CGContextSetFillColor(g, CGColorGetComponents([[UIColor blackColor] CGColor]));
-    CGContextFillPath(g);
+    CGContextAddEllipseInRect(g, CGRectMake(rect.origin.x + CLOCK_BORDER_DISTANCE + (CLOCK_BORDER_SIZE / 2), rect.origin.y + CLOCK_BORDER_DISTANCE + (CLOCK_BORDER_SIZE / 2), rect.size.width - (CLOCK_BORDER_DISTANCE * 2) - CLOCK_BORDER_SIZE, rect.size.height - (CLOCK_BORDER_DISTANCE * 2) - CLOCK_BORDER_SIZE));
+    CGContextSetStrokeColorWithColor(g, [[UIColor blackColor] CGColor]);
+    CGContextSetFillColorWithColor(g, [[UIColor whiteColor] CGColor]);
+    CGContextSetLineWidth(g, 1);
+    CGContextStrokePath(g);
 }
 
 
